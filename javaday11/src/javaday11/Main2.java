34f85f221d5b20a8623828a2181fc0c9acffba63
admin <admin@DESKTOP-SQSQ84J>
2020-01-17 17:45:40 +0900

    20/01/17

---
+++ javaday11/src/javaday11/Main2.java
@@ -0,0 +1,71 @@
+package javaday11;
+
+//하나의 파일에 클래스를 2개 만든 다면
+//public class는 1개이어야 하고 파일이름은 public class의 이름이어야 합니다.
+
+//Runnable 인터페이스를 implements 한 클래스
+class RunnableImpl implements Runnable{
+	//스레드로 수행할 내용을 가지는 메소드
+	public void run() {
+		try {
+			for(int i=0; i<5; i=i+1) {
+				Thread.sleep(1000);
+				System.out.println("Hello Runnable11");
+			}
+		}catch(Exception e) {
+			System.out.println("스레드 예외:"+e.getMessage());
+		}
+		
+	}
+}
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		RunnableImpl r1 = new RunnableImpl();
+		
+		Thread th11 = new Thread(r1);
+		th11.start();
+		
+		
+		//r1 과 r2는 같은 기능
+		Runnable r2 = new Runnable() {
+			public void run() {
+				//Hello Runnable 을 1초마다 5번 출력
+				try {
+					for(int i=0; i<5; i=i+1) {
+						Thread.sleep(1000);
+						System.out.println("Hello Runnable");
+					}
+				}catch(Exception e) {
+					System.out.println("스레드 예외:"+e.getMessage());
+				}
+				
+			}
+		};
+		//Thread 클래스의 인스턴스를 생성
+		Thread th = new Thread(r2);
+		//r2의 run메소드를 스레드로 수행
+		th.start();
+		
+		Thread th1 = new Thread() {
+			public void run() {
+				//Hello Thread 을 1초마다 5번 출력
+				try {
+					for(int i=0; i<5; i=i+1) {
+						Thread.sleep(1000);
+						System.out.println("Hello Thread");
+					}
+				}catch(Exception e) {
+					System.out.println("스레드 예외:"+e.getMessage());
+				}
+				
+			}
+		
+		};
+		//스레드로부터 상속을 받은 경우에는 바로 start() 호출
+		th1.start();
+	
+	}
+
+}
---
+++ javaday11/src/javaday11/ThreadEx.java
@@ -0,0 +1,20 @@
+package javaday11;
+
+public class ThreadEx extends Thread {
+	
+	public void run() {
+		//1초마다 Hello Thread 라는 문자열을 10번 출력
+		for(int i=0; i<10; i=i+1) {
+			System.out.println("Hello Thread");
+			try {
+				//현재 스레드 1초 대기
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
